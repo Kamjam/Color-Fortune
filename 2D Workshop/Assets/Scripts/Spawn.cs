76f727740a49da2f32ec5d28864a19c634a394a6
(╯✧∇✧)╯KamKam <kamillayla@gmail.com>
2024-03-27 00:12:18 -0400

    Full Upload

---
+++ 2D Workshop/Assets/Scripts/Spawn.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    //Ref. to colorswap script
+    private ColorSwap textbox;
+
+    float moveSpeed = -0.02f;
+
+    void Start()
+    {
+        textbox = ColorSwap.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate (0, moveSpeed, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //show the fortune
+        if(other.gameObject.tag == "Boundary")
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+
+            //show the text
+            textbox.showWords();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) 
+    {
+        //destroy itself when it collides with a boundary, and hide the fortune
+        if(other.gameObject.tag == "Boundary")
+        {
+            new WaitForSeconds(120);
+            
+            Destroy(gameObject);
+            //hide the text
+            textbox.hideWords();
+        }
+    }
+}
